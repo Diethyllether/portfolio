a536b4a1fb2bb67e8cd5c84fbcdaf965f56e84f6
Diethyllether <felipecornelio@outlook.com.br>
2025-09-22 21:50:26 -0300

    a lot

---
+++ script.js
@@ -0,0 +1,8 @@
+// God's in his heaven, all's right with the world.
+
+function switchTab(tabName) {
+    const ola = document.getElementById("olaTab");
+    const sobre = document.getElementById("sobreTab");
+    const projetos = document.getElementById("projetosTab");
+    const contato = document.getElementById("contatoTab");
+}
